7de449a9132b36e2102111aadf5db6396c276849
mlelansky <mirko_lelansky@web.de>
2013-11-18 00:04:56 +0100

    Change the build-script to generate html-reports. Add a class with a main-method. Add some additional test for the accesskey-feature.

--- src/test/java/de/fhb/systemintegration/wikipedics/cucumber/features/steps/AccesskeyStepDefinitions.java
+++ src/test/java/de/fhb/systemintegration/wikipedics/cucumber/features/steps/AccesskeyStepDefinitions.java
@@ -1,8 +1,10 @@
 package de.fhb.systemintegration.wikipedics.cucumber.features.steps;
 
+import cucumber.api.java.After;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
+import cucumber.api.java.Before;
 import org.junit.Assert;
 import de.fhb.systemintegration.wikipedics.util.ApplicationRunner;
 
@@ -17,7 +19,17 @@ public final class AccesskeyStepDefinitions {
     /**
      * The test runner object of the application.
      */
-    private ApplicationRunner runner = new ApplicationRunner();
+    private ApplicationRunner runner;
+
+    /**
+     * This method setups the acceptance test.
+     * It initialize the application runner which access the real application.
+     *
+     */
+    @Before
+    public void setUp() {
+        this.runner = new ApplicationRunner();
+    }
 
     /**
      * This method makes some initialisations for the tests.
@@ -28,19 +40,29 @@ public final class AccesskeyStepDefinitions {
     }
 
     /**
-     * This method define the program execution steps.
+     * This method simulate the different user inputs.
+     * @param key the accesskey
      */
-    @When("^the user enter an empty accesskey$")
-    public void enterEmptyAccesskey() {
-        runner.setUserInput("-accesskey=");
+    @When("^the user enter \"([^\"]*)\"$")
+    public void enterAccesskey(final String key) {
+        runner.setUserInput(key);
     }
 
     /**
      * This method define the checks for the scenario.
+     * @param message the message which should be checked
      */
-    @Then("^the user should get a message that the acceskey is to short$")
-    public void validateErrorMessage() {
-        Assert.assertEquals("The accesskey is empty!", runner.getDisplayed());
+    @Then("^the user should get the message \"([^\"]*)\"$")
+    public void validateMessage(final String message) {
+        Assert.assertEquals(message, runner.getDisplayed());
+    }
+
+    /**
+     * This method cleans the test.
+     */
+    @After
+    public void tearDown() {
+        this.runner.close();
     }
 
 }
--- src/test/java/de/fhb/systemintegration/wikipedics/util/ApplicationRunner.java
+++ src/test/java/de/fhb/systemintegration/wikipedics/util/ApplicationRunner.java
@@ -1,12 +1,23 @@
 package de.fhb.systemintegration.wikipedics.util;
 
+import de.fhb.systemintegration.wikipedics.AWSApplication;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.PipedInputStream;
+import java.io.PipedOutputStream;
+import java.io.PrintStream;
+import java.io.UnsupportedEncodingException;
+import java.nio.charset.Charset;
+
 /**
  * This class is a special test runner, that runs the real application.
  * This class controls the standard input and output streams.
  * This implementation based on the implementation
  * from the book "Growning Object-Oriented Software" and of the blog from
  * Sven Grand under http://svengrand.blogspot.de/2011/12/
- * testing-java-console-applications.html?m=1.
+ * testing-java-console-applications.html .
  *
  * @author mlelansky
  * @version 0.0.1
@@ -14,9 +25,77 @@ package de.fhb.systemintegration.wikipedics.util;
 public final class ApplicationRunner {
 
     /**
+     * This field defines the max retry count to fetch the output messages.
+     */
+    private static final int MAX_RETRIES = 20;
+    /**
+     * This field defines the sleep time of the threads.
+     */
+    private static final long MAX_SLEEP_TIME = 100L;
+    /**
+     * This stream connect the outputStream with System.in .
+     */
+    private PipedInputStream inputStream;
+    /**
+     * This is stream stands for System.in .
+     */
+    private PipedOutputStream outputStream;
+    /**
+     * This printStream connect System.out with the byteArrayOutputStream.
+     */
+    private PrintStream printStream;
+    /**
+     * This stream stands for System.out .
+     */
+    private ByteArrayOutputStream byteArrayOutputStream;
+    /**
+     * This is the backup of System.in .
+     */
+    private InputStream systeminBackup;
+    /**
+     * This is the backup of System.out .
+     */
+    private PrintStream systemoutBackup;
+    /**
+     * This is the default charset used for conversation.
+     */
+    private static final Charset DEFAULT_CHARSET = Charset.forName("UTF-8");
+
+    /**
+     * This is the default constructor of the this test runner class.
+     * This class initializes some input/output to manage the user inputs.
+     *
+     */
+    public ApplicationRunner() {
+        try {
+            this.outputStream = new PipedOutputStream();
+            this.inputStream = new PipedInputStream(this.outputStream);
+            this.systeminBackup = System.in;
+            System.setIn(this.inputStream);
+
+            this.byteArrayOutputStream = new ByteArrayOutputStream();
+            this.printStream = new PrintStream(this.byteArrayOutputStream,
+                    true, DEFAULT_CHARSET.name());
+            this.systemoutBackup = System.out;
+            System.setOut(this.printStream);
+        } catch (IOException e) {
+            System.err.println(e.getLocalizedMessage());
+            close();
+        }
+    }
+
+    /**
      * This method starts the real application.
      */
     public void startApplication() {
+        Thread applicationThread = new Thread() {
+            @Override
+            public void run() {
+                AWSApplication.main(new String[]{});
+            }
+        };
+        applicationThread.setDaemon(true);
+        applicationThread.start();
 
     }
 
@@ -25,7 +104,12 @@ public final class ApplicationRunner {
      * @param input the new user input
      */
     public void setUserInput(final String input) {
-
+        try {
+            this.outputStream.write(input.getBytes(Charset.forName("UTF-8")));
+        } catch (IOException e) {
+            System.out.println(e.getLocalizedMessage());
+            close();
+        }
     }
 
     /**
@@ -33,7 +117,50 @@ public final class ApplicationRunner {
      * @return the displayed messages
      */
     public String getDisplayed() {
-        return "The accesskey is empty!";
+        String result = "";
+        int tries = MAX_RETRIES;
+        try {
+            while (tries > 0 && result.length() <= 0) {
+                Thread.sleep(MAX_SLEEP_TIME);
+                result = this.byteArrayOutputStream.toString(
+                        DEFAULT_CHARSET.name());
+                tries--;
+            }
+        } catch (InterruptedException e) {
+            System.err.println(e.getLocalizedMessage());
+        } catch (UnsupportedEncodingException e) {
+            System.err.println(e.getLocalizedMessage());
+        }
+        return result;
+    }
+
+    /**
+     * This method closed opened stream.
+     */
+    public void close() {
+        try {
+            if (this.inputStream != null) {
+                this.inputStream.close();
+                this.inputStream = null;
+            }
+            if (this.outputStream != null) {
+                this.outputStream.close();
+                this.outputStream = null;
+            }
+            if (this.printStream != null) {
+                this.printStream.close();
+                this.printStream = null;
+            }
+            if (this.byteArrayOutputStream != null) {
+                this.byteArrayOutputStream.close();
+                this.byteArrayOutputStream = null;
+            }
+        } catch (IOException e) {
+            System.err.println(e.getLocalizedMessage());
+        } finally {
+            System.setIn(this.systeminBackup);
+            System.setOut(this.systemoutBackup);
+        }
     }
 
 }
